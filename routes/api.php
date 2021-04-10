98a0ec0f809aa5cd3c7fe27fe9e4224889f2b89f
Leandro Cáceres <leandrodamian@hotmail.com>
2021-04-10 16:24:01 -0300

    Config del proyecto

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/test', function (Request $request) {
+    return "VALIDO";
 });
+
+/*
+    Route::prefix('users')->group(function () {
+        Route::post('login', 'UserController@Login')->middleware('loginToken');
+        Route::post('checkLogin', 'UserController@CheckLogin')->middleware('CheckLogin');
+        Route::get('getUsers', 'UserController@GetUsers')->middleware('autentication');
+        Route::get('getUserById', 'UserController@GetUserById')->middleware('autentication');
+        Route::post('createUser', 'UserController@CreateUser')->middleware('autentication');
+        Route::post('setUser', 'UserController@SetUser')->middleware('autentication');
+        Route::post('uploadImg', 'UserController@UploadImage')->middleware('autentication');
+        Route::post('recoverPassword', 'UserController@RecoverPassword');
+        Route::post('resetPassword', 'UserController@ResetPassword');
+    });
+ */
