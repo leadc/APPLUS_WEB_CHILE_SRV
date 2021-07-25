25b000cc410d969453a83d47f91a2b335d76d4dc
Leandro Cáceres <leandrodamian@hotmail.com>
2021-07-25 14:39:58 -0300

    Added format to confirmation email

--- resources/views/mailReserva.blade.php
+++ resources/views/mailReserva.blade.php
@@ -14,6 +14,26 @@
                 padding-bottom: 50px;
                 background-color: #e9ecef;
             }
+            .header{
+                background-color: #FC6500;
+                padding-top: 5px;
+                padding-left: 25px;
+                padding-right: 25px;
+                padding-bottom: 5px;
+            }
+            .content{
+                padding-top: 5px;
+                padding-left: 25px;
+                padding-right: 25px;
+                padding-bottom: 25px;
+            }
+            .footer{
+                background-color: #323234;
+                padding-top: 5px;
+                padding-left: 25px;
+                padding-right: 25px;
+                padding-bottom: 5px;
+            }
             .fc-orange{
                 color: #FC6500;
             }
@@ -32,20 +52,33 @@
     <body>
         <span class="d-none">Confirmación de su reserva de turno para realizar la revisión técnica de su veículo. <br></span>
         <div class="container">
-            <p>
-                Estimado {{ $nombre }} {{ $apellido }}:
-            </p>
-            <p>
-                Su reserva fue confirmada con éxito, a continuación le enviamos los detalles de la misma
-            </p>
-            <ul>
-                <li>Patente: <span class="font-weight-bold">{{ $patente }}</span></li>
-                <li>Código de reserva: <span class="font-weight-bold">{{ $codigo }}</span></li>
-                <li>Día: <span class="font-weight-bold">{{ $fecha }}</span></li>
-                <li>Horario: <span class="font-weight-bold">{{ $hora }}</span></li>
-                <li>Centro: <span class="font-weight-bold">{{ $centro }}</span></li>
-                <li>Dirección: <span class="font-weight-bold">{{ $direccion }}</span></li>
+            <div class="header">
+                <img src="http://applusprt.cl/media/img/LogoEncabezado.png" width="150">
+            </div>
+            <div class="content">
+                <p>
+                    Estimado {{ $nombre }} {{ $apellido }}:
+                </p>
+                <p>
+                    Su reserva fue confirmada con éxito, a continuación le enviamos los detalles de la misma
+                </p>
+                <ul>
+                    <li>Patente: <span class="font-weight-bold">{{ $patente }}</span></li>
+                    <li>Código de reserva: <span class="font-weight-bold">{{ $codigo }}</span></li>
+                    <li>Día: <span class="font-weight-bold"><?= (new DateTime($fecha))->format('d/m/Y') ?></span></li>
+                    <li>Horario: <span class="font-weight-bold">{{ $hora }}</span></li>
+                    <li>Centro: <span class="font-weight-bold">{{ $centro }}</span></li>
+                    <li>Dirección: <span class="font-weight-bold">{{ $direccion }}</span></li>
+                </ul>
+            </div>
+            <div class="footer">
+                <div>
+                    <img src="http://applusprt.cl/media/img/LogoPie.png" width="125">
+                </div>
+                <p>©Applus+ Chile <?= (new DateTime())->format('Y')?></p>
+                <p><a href="http://applusprt.cl">www.applusprt.cl</a></p>
             </div>
         </div>
+        <p style="text-align: center;">Fin mail</p>
     </body>
 </html>
