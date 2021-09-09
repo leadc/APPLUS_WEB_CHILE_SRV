0266da6c4be0d5be5091fd56dda3d332732b8cd8
Leandro Cáceres <leandrodamian@hotmail.com>
2021-09-09 00:27:42 -0300

    Remove debug cookie

--- app/Http/Middleware/DownForMaintenance.php
+++ app/Http/Middleware/DownForMaintenance.php
@@ -16,7 +16,7 @@ class DownForMaintenance
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!!env('DOWN_FOR_MAINTENANCE', false) && (!isset($_COOKIE['debug']) || $_COOKIE['debug'] !== 'develop') ) {
+        if (!!env('DOWN_FOR_MAINTENANCE', false)) {
             return response('', 503);
         }
         return $next($request);
